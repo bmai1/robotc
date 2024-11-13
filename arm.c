89b14c1bfe237714cfb99e81a156ecd51ba3d6d8
Brian Mai <0hrs3g@gmail.com>
2024-11-13 13:39:36 -0800

    add arm_speed parameter

--- arm.c
+++ arm.c
@@ -1,12 +1,11 @@
-void armDown() {
-    // set armMotor to half speed for half a second
-    motor[port2] = -100;
+void armDown(int arm_speed) {
+    motor[port2] = -arm_speed;
     wait1Msec(500);
     motor[port2] = 0;
 }
 
-void armUp() {
-    motor[port2] = 100;
+void armUp(int arm_speed) {
+    motor[port2] = arm_speed;
     wait1Msec(500);
     motor[port2] = 0;
 }
